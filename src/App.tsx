90225d2d8c54420d3c23532c72197a4875cbb868
Le Thanh Phat <23521142@gm.uit.edu.vn>
2026-07-16 00:36:58 +0700

    feat: wire up the /chung-vui route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { RequireAuth } from './components/RequireAuth'
 import { MusicPlayerProvider } from './context/MusicPlayerContext'
 import { HomePage } from './pages/HomePage'
 import { PublicInvite } from './pages/PublicInvite'
+import { SharedInvite } from './pages/SharedInvite'
 import { GuestInvite } from './pages/GuestInvite'
 import { NotFound } from './pages/NotFound'
 import { AdminLogin } from './pages/admin/AdminLogin'
@@ -16,6 +17,7 @@ export function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/thiep-chung/:guestId?" element={<PublicInvite />} />
+          <Route path="/chung-vui" element={<SharedInvite />} />
           <Route path="/thiep/:guestId" element={<GuestInvite />} />
           <Route path="/admin/login" element={<AdminLogin />} />
           <Route
